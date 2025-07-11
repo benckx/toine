1320fb76e3816f4a4bdfd75662cea3855b3dee1c
benckx <benoit.vleminckx@centrica.com>
2025-07-11 17:35:13 +0200

    added slides

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// noinspection JSIgnoredPromiseFromCall
+
 function htmlCollectionToArray(htmlCollection) {
     const array = [];
     for (let i = 0; i < htmlCollection.length; i++) {
@@ -10,29 +12,43 @@ function getElementsByClassNameArray(className) {
     return htmlCollectionToArray(document.getElementsByClassName(className));
 }
 
-function renderSlide(slideId) {
+/**
+ * @param slideId {string}
+ * @param initial {boolean}
+ */
+function renderSlide(slideId, initial = false) {
     const slide = document.getElementById(slideId);
-    if (slide) {
+    if (slide !== null) {
+        // show selected slide
         getElementsByClassNameArray('slides').forEach(slide => {
             slide.classList.remove('visible-slides');
         });
         slide.classList.add('visible-slides');
-        if (sounds.has(slideId)) {
+
+        // play sound
+        if (!initial && sounds.has(slideId)) {
             sounds.get(slideId).play();
         }
+
+        // apply other slide options
+        const options = slideOptions.find(option => option.id === slideId);
+        if (options !== null && options.title !== undefined) {
+            document.getElementsByTagName('title').item(0).innerText = options.title;
+        }
+    } else {
+        console.error(`slide with id ${slideId} not found`);
     }
 }
 
 window.onload = () => {
     getElementsByClassNameArray('step-link')
-        .forEach(link => {
-            link.addEventListener('click', (event) => {
+        .forEach(stepLink => {
+            stepLink.addEventListener('click', (event) => {
                 event.preventDefault();
                 const slideId = event.target.getAttribute('href').substring(1);
                 renderSlide(slideId);
             });
         });
 
-    // show the first slide by default
-    renderSlide(getElementsByClassNameArray('slides')[0].id);
+    renderSlide(FIRST_SLIDE, true);
 };
